2d52221fe9ff2b459935dc21ab7bb795384015fb
eidecker <henry.eidecker@gmail.com>
2018-05-15 20:56:15 +0200

    Added Alert- and AuthenticationService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { UserComponent } from './user/user.component';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './login/login.component';
+import {UserComponent} from './user/user.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {CustomMaterialModule} from "./core/material.module";
 import {AppRoutingModule} from "./core/app.routing.module";
+import {HttpClientModule} from "@angular/common/http";
+import {JwtInterceptor} from "./core/jwt.interceptor";
 
 @NgModule({
   declarations: [
@@ -16,11 +18,13 @@ import {AppRoutingModule} from "./core/app.routing.module";
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     BrowserAnimationsModule,
     CustomMaterialModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [JwtInterceptor],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
