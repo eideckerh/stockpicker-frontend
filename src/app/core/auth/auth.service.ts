ad1c150c019a507c6a54e771416c3bba27738479
eidecker <henry.eidecker@gmail.com>
2018-06-26 20:44:46 +0200

    use backend for login

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { BehaviorSubject } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {Router} from '@angular/router';
+import {BehaviorSubject} from 'rxjs';
 import {User} from "../../user/model/user";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Injectable()
 export class AuthService {
@@ -12,12 +13,22 @@ export class AuthService {
   }
 
   constructor(
+    private http: HttpClient,
     private router: Router
-  ) {}
+  ) {
+  }
 
-  login(user: User){
-    if (user.username !== '' && user.password != '' ) {
+  login(user: User) {
+    if (user.username !== '' && user.password != '') {
       this.loggedIn.next(true);
+      let headers = new HttpHeaders();
+      headers.append("Authorization", "Basic " + btoa(user.username + ":" + user.password));
+      headers.append("Content-Type", "application/x-www-form-urlencoded");
+      let body = JSON.stringify({username: user.username, password: user.password})
+      console.log("performing request")
+      this.http.post('http://localhost:8080/login', body, {headers: headers}).subscribe(
+        result => console.log("test " + result.toString())
+      )
       this.router.navigate(['/']);
     }
   }
